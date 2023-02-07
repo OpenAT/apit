fe8c6d3520b1fa554b4a0509993882e5ce3a513a
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2023-02-07 16:21:42 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/apit_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'apit_config',
+    'summary': '''FS-Online apit instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance apit
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
